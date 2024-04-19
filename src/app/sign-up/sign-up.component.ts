060b3f126a99eeb394340be17834a95745ec3d03
Jyothsna Chaparala <jyothsna@Jyothsnas-MacBook-Air.local>
2024-04-19 12:40:55 -0400

    Routing between login & register pages

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
