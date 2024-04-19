4f81b907fa85f2b64209d19a4e278882c4261f2f
Jyothsna Chaparala <jyothsna@Jyothsnas-MacBook-Air.local>
2024-04-19 17:15:36 -0400

    mail functionality to contact form

--- src/app/contact-us/contact-us.component.spec.ts
+++ src/app/contact-us/contact-us.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FeedbackComponent } from './feedback.component';
+import { ContactUsComponent } from './contact-us.component';
 
-describe('FeedbackComponent', () => {
-  let component: FeedbackComponent;
-  let fixture: ComponentFixture<FeedbackComponent>;
+describe('ContactUsComponent', () => {
+  let component: ContactUsComponent;
+  let fixture: ComponentFixture<ContactUsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FeedbackComponent]
+      imports: [ContactUsComponent]
     })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(FeedbackComponent);
+      .compileComponents();
+
+    fixture = TestBed.createComponent(ContactUsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
