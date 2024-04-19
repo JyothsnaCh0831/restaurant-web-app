43e94e2fabfe3e67059602b12f94940d86b75ff0
Jyothsna Chaparala <jyothsna@Jyothsnas-MacBook-Air.local>
2024-04-19 13:53:32 -0400

    Routing bug fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
-import { NgModule } from '@angular/core';
 import { LayoutWithNavbarComponent } from './layout-with-navbar/layout-with-navbar.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -14,6 +13,7 @@ export const routes: Routes = [
         component: LayoutWithNavbarComponent,
         children: [
             { path: '', component: HomeComponent },
+            { path: 'home', component: HomeComponent },
             { path: 'menu', component: MenuComponent },
             { path: 'contact-us', component: ContactUsComponent },
             // Define other routes here
