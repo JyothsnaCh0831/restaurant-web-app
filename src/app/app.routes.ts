23f57d8a7a136f6ac08155b3e2d384bfad862fc7
Jyothsna Chaparala <jyothsna@Jyothsnas-MacBook-Air.local>
2024-04-22 16:02:29 -0400

    Designed feedback form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { LayoutWithNavbarComponent } from './layout-with-navbar/layout-with-navbar.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 export const routes: Routes = [
     // Routes with navbar
@@ -15,6 +16,7 @@ export const routes: Routes = [
             { path: '', component: HomeComponent },
             { path: 'home', component: HomeComponent },
             { path: 'menu', component: MenuComponent },
+            { path: 'feedback', component: FeedbackComponent },
             { path: 'contact-us', component: ContactUsComponent },
             // Define other routes here
         ]
